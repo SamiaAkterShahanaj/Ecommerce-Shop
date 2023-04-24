0351d47ae90cc7b53aec66ac689f002ce4b3d2f6
SamiaAkterShahanaj <128840241+SamiaAkterShahanaj@users.noreply.github.com>
2023-04-24 22:12:18 +0600

    first commit

---
+++ ec/app/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+
+@admin.register(Product)
+class ProductModelsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'discounted_price', 'category', 'product_image']
+
+
+@admin.register(Customer)
+class CustomerModelsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'locality', 'city', 'state', 'zipcode']
+
+
+#@admin.register(Cart)
+#class CartModelAdmin(admin.ModelAdmin):
+ #   list_display = ['id', 'user', 'product', 'quantity']
+
